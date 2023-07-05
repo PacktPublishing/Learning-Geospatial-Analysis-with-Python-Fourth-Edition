bcc2f761960d77057773b7040307538d48b6f2f5
GeospatialPython.com <jlawhead@geospatialpython.com>
2023-07-05 09:47:10 -0500

    Update B19730_04_02-urlretrieve.py
    Updated data file url

--- B19730_04_02-urlretrieve.py
+++ B19730_04_02-urlretrieve.py
@@ -2,6 +2,6 @@
 import urllib.request
 import urllib.parse
 import urllib.error
-url = "https://github.com/GeospatialPython/Learning/raw/master/hancock.zip"
+url = "https://github.com/PacktPublishing/Learning-Geospatial-Analysis-with-Python-4th-Edition/raw/main/B19730_02_Asset_Files/hancock.zip"
 fileName = "hancock.zip"
 urllib.request.urlretrieve(url, fileName)
