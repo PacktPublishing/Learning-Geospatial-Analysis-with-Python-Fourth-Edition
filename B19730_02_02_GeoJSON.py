4dfd42a26e8ecab63e921cb0beeeacce61887466
GeospatialPython.com <jlawhead@geospatialpython.com>
2023-07-05 09:54:06 -0500

    Added Chapter 2 code files
    Added code files from chapter 2

---
+++ B19730_02_02_GeoJSON.py
@@ -0,0 +1,18 @@
+gc = { "type": "GeometryCollection", 
+   "geometries": [ 
+     { "type": "Point", 
+       "coordinates": [ 
+         [-89.33,30]] 
+     }, 
+     {"type": "LineString", 
+       "coordinates": [ 
+         [-89.33,30.3],[-89.36,30.28]], 
+     }, 
+     { "type": "Polygon", 
+       "coordinates": [ 
+          [-89.33167167,29.96],[-89.25914630,29.96], [ 
+	        [-89.25914630,30],[-89.33167167,30]], 
+     }] 
+ } 
+
+print(gc)
