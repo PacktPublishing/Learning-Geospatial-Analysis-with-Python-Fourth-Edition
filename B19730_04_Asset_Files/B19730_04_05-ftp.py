9a2c1b934d9472bdaa96a596cce5e22b3f291ff1
GeospatialPython.com <jlawhead@geospatialpython.com>
2025-04-29 21:30:23 -0500

    Update B19730_04_05-ftp.py
    Fixed secure FTP issue due to changes in the NOAA server. The urllib part of the example will no longer work.

--- B19730_04_Asset_Files/B19730_04_05-ftp.py
+++ B19730_04_Asset_Files/B19730_04_05-ftp.py
@@ -1,14 +1,23 @@
 # Read NOAA weather buoy location data via ftp
+
+# UPDATE Apr. 29, 2025: NOAA changed this to an encrypted
+# FTP server. The first part of the example has been updated
+# to accomodate, but will be different from the book until
+# I can get Packt to update it. Thank you to Ryan Burnside
+# for bringing this to my attention. There's a lot of changes
+# occuring in the US government right now, especially at NOAA.
+# - Joel Lawhead
+
 import ftplib
 server = "ftp.pmel.noaa.gov"
 dir = "taodata"
 fileName = "taobuoypos.dat"
-ftp = ftplib.FTP(server)
+ftp = ftplib.FTP_TLS(server)
 ftp.login()
+ftp.prot_p()
 ftp.cwd(dir)
 with open(fileName, "wb") as out:
-    ftp.retrbinary(f"RETR {fileName}", out.write)
-    ftp.quit()
+    ftp.retrbinary("RETR " + fileName, out.write)
 
 with open(fileName) as tao:
     buoy = tao.readlines()[5]
@@ -16,10 +25,15 @@ with open(fileName) as tao:
     print(f"Buoy {loc[0]} is located at {' '.join(loc[4:8])}")
 
 # Now do the same thing with urllib
+# UPDATE Apr. 29, 2025: urllib can't handle encrypted ftp which
+# NOAA has switched to. This example will be updated.
+#  
 
+"""
 import urllib.request
 
-tao = urllib.request.urlopen(f"ftp://{server}/{dir}/{fileName}")
+tao = urllib.request.urlopen("ftp://" + server + "/" + dir + "/" + fileName)
 buoy = str(tao.readlines()[5], encoding="utf8")
 loc = buoy.split()
-print(f"Buoy {loc[0]} is located at {' '.join(loc[4:8])}")
+print(f"Buoy {loc[0]} is located at {' '.join(loc[4:8])}")"
+"""
